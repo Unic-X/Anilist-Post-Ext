7129c6ebdbb715e6a13fbb4ba1c537420ec891a3
Unic X <73340774+Unic-X@users.noreply.github.com>
2021-05-19 13:18:34 +0530

    idk

--- content.js
+++ content.js
@@ -26,7 +26,9 @@ class AnimePahe{
     isLoaded() {
         document.querySelector(".click-to-load").addEventListener("click",()=>
         {
-            console.log("player is now loaded");
+            console.log(`%c Player is Now Loaded!', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 
+                        6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 
+                        18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)`);
             return true;
     })
     }
@@ -41,10 +43,9 @@ class Gogoanime{
 
     gogoanimeEpisode(str){
         //SEIJO NO MARYOKU WA BANNOU DESU EPISODE 7 ENGLISH SUBBED
-        console.log("hello world");
-        console.log(title);
         let [animeName,strEp]=str.split("Episode");
         const intEp=parseInt(strEp);
+        this.name=animeName;this.episode=intEp;
         return({
             animeName,intEp
         })
@@ -79,11 +80,12 @@ function alogund(){
 function toAnimeName(w){
     if(alogund() instanceof AnimePahe ){
         let a=alogund().animepaheNameEpisode(w);
-        console.log(a.name+" "+a.intEp);
+        console.log(a.name+"|"+a.intEp);
         return a.name;
     }else if(alogund() instanceof Gogoanime){
-        let a=alogund();
-        console.log(a.gogoanimeEpisode(w))
+        let a=alogund().gogoanimeEpisode(w);
+        console.log(a.animeName+"|"+a.intEp)
+        return a.animeName
     }else{
         //pass
     }
