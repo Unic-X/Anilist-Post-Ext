e1f30384aed3eb872cd59a9cf1084c11edf91e36
Unic X <73340774+Unic-X@users.noreply.github.com>
2021-06-11 18:16:02 +0530

    Sharing data between Content.js and bg.js

--- bg.js
+++ bg.js
@@ -1,7 +1,18 @@
+let acha={
+    key:null,
+    coverImage:null,
+    titleRomaji:null
+  }
+  chrome.storage.sync.set(acha, function() {
+    console.log('Value is set to ' + acha);
+  })
 
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    console.log(tabs[0].url);
-});
-chrome.tabs.onActivated.addListener(tab=>{
-    console.log(tab.url)
-});
+
+`Changes the storage values when any change from content.js`
+chrome.storage.onChanged.addListener((changes)=>{
+    console.log(changes)
+    chrome.storage.sync.get(['key','coverImage',"titleRomaji"], function(result) {
+        console.log('Value currently is ' + result.key + "coverImage: "+result.coverImage);
+        acha.key=result.key;acha.coverImage=result.coverImage;acha.titleRomaji;
+    });
+})
--- content.js
+++ content.js
@@ -2,7 +2,6 @@ var title = document.getElementsByTagName("title")[0].innerHTML;
 
 
 
-
 class AnimePahe {
   constructor() {
     this.name = null;
@@ -29,9 +28,7 @@ class AnimePahe {
 
   onLoaded() {
     document.querySelector(".click-to-load").addEventListener("click", () => {
-      console.log(`%c Player is Now Loaded!', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 
-                        6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 
-                        18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)`);
+      console.log('%c Player is Now Loaded!', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
       return true;
     });
   }
@@ -67,7 +64,7 @@ function containsURL(str) {
 
 //Not a good name but checks which site and returns the instance of the site class wrapper
 function alogund() {
-  if (containsURL("animepahe.org")  || containsURL("animepahe.com")) {
+  if (containsURL("animepahe.org") || containsURL("animepahe.com")) {
     return new AnimePahe();
   } else if (containsURL("gogoanime.ai")) {
     try {
@@ -157,7 +154,19 @@ async function chutiya() {
     .catch((error) => {
       console.warn(error);
     });
-  console.log(a);
+
+    let acha={
+      key:a.data.Media.description,
+      coverImage:a.data.Media.coverImage.large,
+      titleRomaji:a.data.Media.title.english
+    }
+
+    chrome.storage.sync.set(acha, function() {
+      console.log('Value is set to ' + acha);
+    });
+    chrome.storage.sync.get(['key','coverImage'], function(result) {
+    console.log('Value currently is ' + result.key + "coverImage: "+result.coverImage);
+});
 }
 
 Promise.resolve(chutiya());
