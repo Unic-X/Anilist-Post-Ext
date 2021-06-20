78b10a02138792bdfeee37a9790ca8b4c8882f91
Unic X <73340774+Unic-X@users.noreply.github.com>
2021-06-20 16:18:28 +0530

    a.

--- popup.js
+++ popup.js
@@ -1,46 +1,53 @@
 var paadu={
     key:null,
-    coverImage:null
+    coverImage:null,
+    titleEnglish:null,
 }
-const signInButton=document.getElementById("sign_in");
+//const signInButton=document.getElementById("sign_in");
 
 
-signInButton.addEventListener("click",()=>{
+/*signInButton.addEventListener("click",()=>{
     chrome.tabs.create({ url: "https://anilist.co/api/v2/oauth/authorize?client_id=5474&response_type=token" })
-})
+})*/
 
 function allah(a,b){
-    document.getElementById("chamar").innerHTML=a;
-    document.getElementById("chamar_image").style.backgroundImage=`url(${b})`;
+    if(a.length>14){
+        document.getElementById("animeTitle").innerHTML=a.slice(0,11)+"...";
+    }else{
+        document.getElementById("animeTitle").innerHTML=a;
+    }
+    document.getElementById("animeNameFull").innerHTML=a;
+    document.getElementById("coverImg").style.backgroundImage=`url(${b})`;
 }
-chrome.storage.sync.get(['key','coverImage'], function(result) {
-    paadu.coverImage=result.coverImage;paadu.key=result.key;
-    allah(result.key,result.coverImage);
+chrome.storage.sync.get(['titleEnglish','coverImage'], function(result) {
+    paadu.coverImage=result.coverImage;paadu.titleEnglish=result.titleEnglish;
+    allah(result.titleEnglish,result.coverImage);
 })
 
 var acha={
     key:null,
-    coverImage:null
+    coverImage:null,
+    titleEnglish:null,
 };
 
 
 `Changes the storage values when any change from content.js`
 chrome.storage.onChanged.addListener(()=>{
-    chrome.storage.sync.get(['key','coverImage'], function(result) {
-        acha.key=result.key;acha.coverImage=result.coverImage;
-        allah(result.key,result.coverImage);
+    chrome.storage.sync.get(['titleEnglish','coverImage'], function(result) {
+        acha.titleEnglish=result.titleEnglish;acha.coverImage=result.coverImage;
+        allah(result.titleEnglish,result.coverImage);
     });
 })
 
-const changeLang=document.getElementById("sign_in");
+//const changeLang=document.getElementById("sign_in");
 
 
-function toLang(){
+/*function toLang(){
     changeLang.innerHTML=`Current Language is (${currentLang})`;
     changeLang.addEventListener("click",()=>{
         
     })
-}
+}*/
 
 
 
