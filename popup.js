0d6f6ee57473a0ec136b7b72b19daf0d2709783e
Unic X <73340774+Unic-X@users.noreply.github.com>
2021-06-20 21:24:25 +0530

    Added some functions idk

--- popup.js
+++ popup.js
@@ -2,13 +2,40 @@ var paadu={
     key:null,
     coverImage:null,
     titleEnglish:null,
-}
-//const signInButton=document.getElementById("sign_in");
+};
+const signInButton=document.getElementById("sign_in");
+
+const removeEpWatched = document.getElementById("lastEpMinus");
+
+const addEpWatched = document.getElementById("lastEpPlus");
+
+addEpWatched.addEventListener("click",()=>{
+    const inputEpWatched = document.getElementById("inputEpWatched");
+    var c = parseInt(inputEpWatched.value);
+    if(isNaN(c)){
+        c=0
+    }
+    c++;
+    inputEpWatched.value = c;
+    inputEpWatched.innerHTML = c;
+})
+
+removeEpWatched.addEventListener("click",()=>{
+    const inputEpWatched = document.getElementById("inputEpWatched");
+    var c = parseInt(inputEpWatched.value);
+    if(isNaN(c) || c===0){
+        return
+    }
+    c--;
+    inputEpWatched.value = c;
+    inputEpWatched.innerHTML = c;
+})
 
 
-/*signInButton.addEventListener("click",()=>{
+
+signInButton.addEventListener("click",()=>{
     chrome.tabs.create({ url: "https://anilist.co/api/v2/oauth/authorize?client_id=5474&response_type=token" })
-})*/
+});
 
 function allah(a,b){
     if(a.length>14){
@@ -18,11 +45,15 @@ function allah(a,b){
     }
     document.getElementById("animeNameFull").innerHTML=a;
     document.getElementById("coverImg").style.backgroundImage=`url(${b})`;
-}
-chrome.storage.sync.get(['titleEnglish','coverImage'], function(result) {
+};
+chrome.storage.sync.get(['titleEnglish','coverImage',"auth_token"], function(result) {
+    if("auth_token" in result){
+        signInButton.innerHTML="Log Out";
+        signInButton.id="signed_in";
+    }
     paadu.coverImage=result.coverImage;paadu.titleEnglish=result.titleEnglish;
     allah(result.titleEnglish,result.coverImage);
-})
+});
 
 var acha={
     key:null,
@@ -38,6 +69,9 @@ chrome.storage.onChanged.addListener(()=>{
         allah(result.titleEnglish,result.coverImage);
     });
 })
+
+
+
 
 //const changeLang=document.getElementById("sign_in");
 
